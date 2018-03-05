322a47d09380664c9c89c5647bbf3b1bdaaa6d97
suetAndTie <trtets@gmail.com>
2018-03-05 15:44:41 -0800

    testing rewritten code

--- constants.py
+++ constants.py
@@ -40,6 +40,8 @@ BATCH_SIZE = 8
 LR = 0.0002
 # Beta options for the Adam Optimizer
 BETAS = (0.5, 0.999)
+# Size for each dimension of the image
+IMAGE_SIZE = 128
 
 
 # Options for the main model
@@ -47,7 +49,7 @@ MAIN_MODEL_OPTIONS = {
     'caption_vec_len':4800,     # Dimensions for the embedded captions vector
     't_dim':256,                # Dimensions for the text vector input into the GAN
     'z_dim':100,                # Dimensions for the noise vector input into the GAN
-    'image_size':128,           # Number of pixels in each dimension of the image
+    'image_size':IMAGE_SIZE,           # Number of pixels in each dimension of the image
     'num_gf':64,                # Number of generator filters in first layer of generator
     'num_df':64,                # Number of discriminator filters in first layer of discriminator
     'image_channels':3,         # Number of channels for the output of the generator and input of discriminator
